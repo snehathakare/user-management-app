d4049d1996a9d73c1eb02e3a081dd9ccde66dae1
Sneha <snhthakare@gmail.com>
2022-10-28 17:19:26 +0100

    Adds search component

---
+++ src/components/Search.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TextField from '@mui/material/TextField'
+
+const Search = () => {
+    return (
+        <TextField fullWidth id="outlined-basic" label="Search" variant="outlined" />
+    )
+}
+
+export default Search
