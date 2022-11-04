7f322336c2ad4f46ac712f5e30dd13cf0d7994ca
Sneha <snhthakare@gmail.com>
2022-11-04 12:06:09 +0000

    Adds redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { applyMiddleware } from 'redux';
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from 'redux-thunk';
+import rootReducer from './../reducers/index.js';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = configureStore(
+  { reducer: rootReducer },
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
