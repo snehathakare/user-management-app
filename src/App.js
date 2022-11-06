0e2bc958ab18a926fd71763817a0f3e0a290e628
Sneha <snhthakare@gmail.com>
2022-11-06 17:43:41 +0000

    Adds routes to app file

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import './App.css';
 import Home from './pages/Home';
 import UserDetails from './pages/UserDetails';
 import GroupDetails from './pages/GroupDetails';
+import UpdateGroup from './components/Groups/UpdateGroup';
+import ShowUser from './components/Users/ShowUser';
+import UpdateUser from './components/Users/UpdateUser';
+import ShowGroup from './components/Groups/ShowGroup';
 
 function App() {
   return (
@@ -15,7 +19,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}>
           <Route path="/users" element={<UserDetails />} />
+          <Route path="/users/:id" element={<ShowUser />} />
+          <Route path="/users/new" element={<ShowUser />} />
+          <Route path="/users/edit/:id" element={<UpdateUser />} />
           <Route path="/groups" element={<GroupDetails />} />
+          <Route path="/groups/:id" element={<ShowGroup />} />
+          <Route path="/groups/new" element={<ShowGroup />} />
+          <Route path="/groups/edit/:id" element={<UpdateGroup />} />
         </Route>
       </Routes>
     </BrowserRouter>
