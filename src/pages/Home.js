068a76dab47350e0366539b52334682c0fa1f180
Sneha <snhthakare@gmail.com>
2022-11-06 17:44:17 +0000

    Adds heading to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import NavBar from '../components/NavBar'
-import Search from '../components/Search'
 import Footer from '../components/Footer'
+import { Box, Typography } from '@mui/material'
+import { Stack } from '@mui/system'
 
 const Home = () => {
     return (
-        <div>
+        <Box>
             <NavBar />
-            <Search />
+            <Stack textAlign='center'>
+                <Typography variant='h6'>
+                    Welcome to user management system!
+                </Typography>
+                <Typography variant='body2'>
+                    Use the navbar links to explore the website
+                </Typography>
+            </Stack>
             <Outlet />
             <Footer />
-        </div>
+        </Box >
     )
 }
 
